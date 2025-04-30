b8de4b258130e8881bfd595e4594766135c587bd
jotapelopes <jp.santoslopes28@gmail.com>
2025-04-30 10:50:59 -0300

    Subindo tudo

--- README.md
+++ README.md
@@ -1,28 +1,7 @@
-# 🌲 Cypress, do Zero à Nuvem ☁️
+# Cypress do zero a nuvem
 
-👋 Seja bem-vindo(a)!
-
-É muito bom tê-lo(a) aqui. Tenho certeza que você vai amar esse curso. ❤️
-
-## O que você vai aprender?
-
-- Como configurar um projeto Cypress do zero
-- Como visitar páginas locais e remotas
-- Como lidar com os elementos mais comuns encontrados em aplicações web
-- Como testar upload de arquivos
-- Como realizar as mais diversas verificações de resultados esperados
-- Como criar comandos customizados
-- Como lidar com links que abrem em outra aba do navegador
-- Como rodar testes simulando as dimensões de um dispositivo móvel
-- Como resolver os mesmos problemas de diferentes formas, conhecendo a [API do Cypress](https://docs.cypress.io/api/table-of-contents)
-- Como criar uma documentação mínima para seu projeto de testes automatizados
-- Como executar os testes em um _workflow_ de integração contínua sempre que mudanças ocorrerem no código da aplicação (ou dos testes)
-- Como integrar seu _workflow_ de integração contínua com o Cypress Cloud (o serviço de gestão de testes do Cypress na nuvem)
-
-## Vamos começar?
-
-Vá para a seção [estrutura do curso](./lessons/_course-structure_.md).
-
-___
-
-Este é um curso da **Escola Talking About Testing**.
+Uma breve descrição do que trata o projeto
+Pré-requisitos (tais como Node.js, npm, git, etc.)
+Passos para instalação das dependências
+Passos para rodar os testes
+Qualquer outra informação que for pertinente
--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -41,7 +41,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#firstName').type('João')
     cy.get('#lastName').type('Pedro')
     cy.get('#email').type('joao.lopes@gmail.com')
-    cy.get('#phone-checkbox').click()
+    cy.get('#phone-checkbox').check()
     cy.get('#open-text-area').type('abcdefghijklmnopqrstuvwxyz', {delay: 0})
     cy.contains('button', 'Enviar').click()
     //Resultado esperado
@@ -84,4 +84,86 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('.success').should('be.visible')
   })
 
-})
+//  Lesson 3
+
+  it('Seleciona um produto (YouTube) por seu texto', () => {
+    cy.get('#product')
+      .select('YouTube')
+      .should('have.value', 'youtube')
+  })
+
+  it('Seleciona um produto (Mentoria) por seu valor', () => {
+    cy.get('#product')
+      .select('mentoria')
+      .should('have.value', 'mentoria')
+  })
+
+  it('Seleciona um produto (Blog) por seu valor', () => {
+    cy.get('#product')
+      .select(1)
+      .should('have.value', 'blog')
+  })
+
+  it('Marca o tipo de atendimento "Feedback"', () => {
+    cy.get('input[type="radio"][value="feedback"]')
+      .click()
+      .should('be.checked')
+  })
+
+  it('Marca cada tipo de atendimento', () => {
+    cy.get('input[type="radio"]')
+      .each( typeOfService => { 
+        cy.wrap(typeOfService)
+        .check()
+        .should('be.checked')
+      })
+  })
+
+  it('Marca ambos os checkboxes, depois desmarca o útlimo', () => {
+    cy.get('input[type="checkbox"]')
+      .check()
+      .should('be.checked')
+      .last()
+      .uncheck()
+      .should('not.be.checked')      
+  })
+
+  it('Seleciona um arquivo da pasta fixtures', () => {
+    cy.get('#file-upload')
+      .selectFile("cypress/fixtures/example.json")
+      .should((input) => {
+        expect(input[0].files[0].name).to.equal('example.json')
+      })
+  })
+
+  it('Seleciona um arquivo simulando um drag-and-drop', () => {
+    cy.get('#file-upload')
+      .selectFile("cypress/fixtures/example.json", { action: 'drag-drop' }) // Simula o usuário arrastando o arquivo para o campo de inserção
+      .should((input) => {
+        expect(input[0].files[0].name).to.equal('example.json')
+      })
+  })
+
+  it('Seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+    cy.fixture('example.json')
+      .as('sampleFile')
+    cy.get('#file-upload')
+      .selectFile("@sampleFile", { action: 'drag-drop' })
+      .should((input) => {
+        expect(input[0].files[0].name).to.equal('example.json')
+      })
+
+  it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+    cy.contains('a', 'Política de Privacidade')
+      .should('have.attr', 'href', 'privacy.html')
+      .and('have.attr', 'target', '_blank')
+    })
+  })
+
+  it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+    cy.contains('a', 'Política de Privacidade')
+      .invoke('removeAttr', 'target')
+      .click()
+    cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
+    })
+  })
